388508b038aa511e994b8991f4f3598b0677ac8c
Sander Willems <sander.willems@ugent.be>
2020-07-01 15:56:49 +0200

    Testing bugfix

--- install/test.sh
+++ install/test.sh
@@ -6,6 +6,6 @@ tar xf ion_networks/install/test_data.tar.xz --directory ion_networks/projects
 eval "$(conda shell.bash hook)"
 ion_networks_command="$(conda activate ion_networks; which python)"
 ion_networks='"${ion_networks_command}" "$(pwd)"/ion_networks/src/ion_networks.py'
-"$ion_networks" create -i ion_networks/projects/test_data -l ion_networks/projects/test_data
-"$ion_networks" evidence -i ion_networks/projects/test_data -l ion_networks/projects/test_data
-"$ion_networks" show
+eval "$ion_networks" create -i ion_networks/projects/test_data -l ion_networks/projects/test_data
+eval "$ion_networks" evidence -i ion_networks/projects/test_data -l ion_networks/projects/test_data
+eval "$ion_networks" show
