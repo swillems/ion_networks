d45c3054cea2e62d9cd7afd2533b3dd09f2a7bb3
Sander Willems <sander.willems@ugent.be>
2020-07-02 17:19:21 +0200

    test UX

--- install/test.sh
+++ install/test.sh
@@ -8,4 +8,6 @@ ion_networks_command="$(conda activate ion_networks; which python)"
 ion_networks='"${ion_networks_command}" "$(pwd)"/ion_networks/src/ion_networks.py'
 eval "$ion_networks" create -i ion_networks/projects/test_data -l ion_networks/projects/test_data
 eval "$ion_networks" evidence -i ion_networks/projects/test_data -l ion_networks/projects/test_data
+cd ion_networks/projects
 eval "$ion_networks" show
+cd ../..
