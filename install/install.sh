a2b400824f62539307ee1cb8ab854ae246802945
Sander Willems <swillems@Sanders-MacBook-Pro.local>
2020-08-13 16:47:14 +0200

    Typo

--- install/install.sh
+++ install/install.sh
@@ -29,12 +29,13 @@ if ! hash ion_networks.py 2>/dev/null; then
   fi
   echo "Installing ion-networks."
   conda env create --file ion_networks/install/environment.yml
-  echo "Adding ion-networks.py alias to ~/.bashrc."
   eval "$(conda shell.bash hook)"
   ion_networks_command="$(conda activate ion_networks; which python)"
   if [ -n "$ZSH_VERSION" ]; then
+     echo "Adding ion-networks.py alias to ~/.zshrc."
      echo "alias ion_networks.py='"${ion_networks_command}" "$(pwd)"/ion_networks/src/ion_networks.py'" >> ~/.zshrc
   elif [ -n "$BASH_VERSION" ]; then
+     echo "Adding ion-networks.py alias to ~/.bashrc."
      echo "alias ion_networks.py='"${ion_networks_command}" "$(pwd)"/ion_networks/src/ion_networks.py'" >> ~/.bashrc
   else
      echo "Unknown shell."
