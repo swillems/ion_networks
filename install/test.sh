11422664d01b83b18328240a6250bdf8713a4489
Sander Willems <swillems@users.noreply.github.com>
2020-08-19 16:50:15 +0200

    Update test.sh

--- install/test.sh
+++ install/test.sh
@@ -4,8 +4,7 @@ echo "Testing installation"
 mkdir ion_networks/projects
 tar xf ion_networks/install/test_data.tar.xz --directory ion_networks/projects
 eval "$(conda shell.bash hook)"
-ion_networks_command="$(conda activate ion_networks; which python)"
-ion_networks='"${ion_networks_command}" "$(pwd)"/ion_networks/src/ion_networks.py'
-eval "$ion_networks" create -i ion_networks/projects/test_data -l ion_networks/projects/test_data
-eval "$ion_networks" evidence -i ion_networks/projects/test_data -l ion_networks/projects/test_data
-eval "$ion_networks" show
+ion_networks_bin="$(conda activate ion_networks; which ion_networks)"
+eval "$ion_networks_bin" create -i ion_networks/projects/test_data -l ion_networks/projects/test_data
+eval "$ion_networks_bin" evidence -i ion_networks/projects/test_data -l ion_networks/projects/test_data
+eval "$ion_networks_bin" show
